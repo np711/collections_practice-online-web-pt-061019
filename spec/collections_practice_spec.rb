8a379a74033ec60ee4fac9d8edb0690c0fb9a111
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-06 18:23:18 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'collections practice' do
 
   # Question 1
-  describe '#sort_array_asc' do
+  describe 'sort_array_asc' do
     it 'should return an array sorted in ascending order' do
       expect(sort_array_asc([25, 7, 1])).to eq([1,7,25])
     end
